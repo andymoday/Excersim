8ce83a761b8b35d29f49f219b2036bf1466a97c2
Andy Moday <andy.mopad@gmail.com>
2021-07-15 18:14:11 +0100

    correct isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,5 +1,8 @@
 def is_isogram(string):
+    # ensuring case is irrelevant in character count
+    string = string.lower()
     for char in string:
-        if string.count(char) > 1:
+        # excludes whitespace, punctuation etc.
+        if string.count(char) > 1 and char.isalpha():
             return False
     return True
